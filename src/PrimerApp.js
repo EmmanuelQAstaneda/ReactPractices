6f7ee93ad19eaf33fdf5704b9ff502a0a1fd13e3
EmmanuelQAstaneda <eliascastaneda.im@gmail.com>
2022-01-07 17:04:23 -0600

    Ready to accept

--- src/PrimerApp.js
+++ src/PrimerApp.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 import hitssImage from "./globalhitss.png";
 
 const PrimerApp = ({ titulo, subtitulo }) => {
-  let value = 0;
+  let value = 101;
   let [counter, setCounter] = useState(value);
 
   function getRandomArbitrary() {
-    setCounter(Math.floor(Math.random() * (30 - 1) + 1));
+    setCounter(Math.floor(Math.random() * (32 - 0) + 0));
   }
   return (
     <Fragment>
@@ -17,10 +17,10 @@ const PrimerApp = ({ titulo, subtitulo }) => {
       <table>
         <tr>
           <th>
-            <button onClick={getRandomArbitrary}>Numero Ganador:</button>
+            <button onClick={getRandomArbitrary}>Nombre del Ganador:</button>
           </th>
           <th>
-            <p1>{counter}</p1>
+            <p1>{nombres[counter]}</p1>
           </th>
         </tr>
       </table>
@@ -38,4 +38,40 @@ PrimerApp.defaultProps = {
 PrimerApp.propTypes = {
   value: PropTypes.number,
 };
+
 export default PrimerApp;
+
+let nombres = [
+  "Mariana Lucero Cruz Mendoza",
+  "Alberto Octavio Ramirez Gomez",
+  "Alejandro Orlando Dagio Lopez",
+  "Orlando Alberto Silva Mendoza ",
+  "Vannya Esnif Fuentes Mora",
+  "Emmanuel Elias Castañeda",
+  "It Zel Lamas Fernandez",
+  "Jesus Enrique Manzano Alvarez",
+  "Jorge Luna Pineda",
+  "Roberto Javier Fuentes Mora",
+  "Angel Uriel Peyrani Chavez",
+  "Cecilia Mariela Martinez Reyes",
+  "Olivia Araceli Herrera Reyes",
+  "Oscar Gael Luna Barron",
+  "Luis Orlando Mendiola Beristain",
+  "Janet Mucino Nava",
+  "Jesus Manuel Razo Vazquez",
+  "Edilia Alcantara Gomez",
+  "Brandon Martinez Hernandez",
+  "David Perez Couto",
+  "Hector Yair Gomez Rodriguez",
+  "Oscar Moreno Rangel",
+  "Lizbeth Leon Angeles",
+  "Edith Lopez Salvador",
+  "Eduardo De La Cruz Hernandez",
+  "Jose Arellano Sanchez",
+  "Lesly Viridiana Tovar Morales ",
+  "Israel Fernando Fragoso Aguilar",
+  "Estephane Joselyn Ramirez Mexicano ",
+  "Carlos Diego Colexcua",
+  "Alan Dahian Medina Herrera",
+  "Francisco Javier De La Cruz Hernandez ",
+];
